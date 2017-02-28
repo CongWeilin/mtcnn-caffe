84e4c866b78a17e7d441feecad2815687620bbf5
CongWeilin <cong_weilin@qq.com>
2017-02-28 21:35:47 +0800

    update 48net

--- 48net/runcaffe.sh
+++ 48net/runcaffe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-
+export PYTHONPATH=$PYTHONPATH:/home/cmcc/caffe-master/examples/mtcnn-caffe/48net
 set -e
-~/caffe/build/tools/caffe train \
+~/caffe-master/build/tools/caffe train \
 	 --solver=./solver.prototxt \
-  	 --weights=./48net-cls-and-roi.caffemodel
+  	 #--weights=./48net-only-cls.caffemodel
